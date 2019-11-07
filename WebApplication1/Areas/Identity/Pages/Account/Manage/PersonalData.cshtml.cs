8406201dfedd123e40ada775e31f893d01269ab5
hashibulkhondokar <hashibulkhondokar@HASHIBULKHO5995>
2019-11-07 16:08:25 +0900

    Management Layout fix

--- WebApplication1/Areas/Identity/Pages/Account/Manage/PersonalData.cshtml.cs
+++ WebApplication1/Areas/Identity/Pages/Account/Manage/PersonalData.cshtml.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -22,6 +24,10 @@ namespace WebApplication1.Areas.Identity.Pages.Account.Manage
         public async Task<IActionResult> OnGet()
         {
             var user = await _userManager.GetUserAsync(User);
+
+            
+
+
             if (user == null)
             {
                 return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
--- WebApplication1/Areas/Identity/Pages/Account/RegisterCustomer.cshtml.cs
+++ WebApplication1/Areas/Identity/Pages/Account/RegisterCustomer.cshtml.cs
@@ -14,7 +14,7 @@ using WebApplication1.Models;
 
 namespace WebApplication1.Areas.Identity.Pages.Account
 {
-    [AllowAnonymous]
+    [Authorize("Admin")]
     public class RegisterCustomerModel : PageModel
     {
         private readonly SignInManager<IdentityUser> _signInManager;
