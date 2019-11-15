e43b4d5bd1645500e9c89f520302cb81140e7ad1
hashibulkhondokar <hashibulkhondokar@HASHIBULKHO5995>
2019-11-15 17:12:58 +0900

    fixed database

---
+++ WebApplication1/Models/Material.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace WebApplication1.Models
+{
+    public class Material : BaseEntity
+    {
+        [Required(ErrorMessage = "")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "")]
+        public string ImageUrl { get; set; }
+
+        public IdentityUser User { get; set; }
+        [ForeignKey("User")]
+        public string UserId { get; set; }
+
+        public Material()
+        {
+
+        }
+
+        public Material(string name, string imageUrl, string userId)
+        {
+            Name = name;
+            ImageUrl = imageUrl;
+            UserId = userId;
+        }
+    }
+}
